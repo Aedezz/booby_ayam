e2d1593bbeb03722c0bbd7145a45f10fd41030f9
Aedezz <wafielegend2017@gmail.com>
2026-08-27 23:12:03 +0800

    awewo

--- data.js
+++ data.js
@@ -1,38 +1,36 @@
 const menuData = [
-    // CRISPY ORIGINAL
-    { id: 'ori_sayap', category: 'Ayam Crispy Original', name: 'Sayap Original', price: 9000 },
-    { id: 'ori_pahabawah', category: 'Ayam Crispy Original', name: 'Paha Bawah Original', price: 10000 },
-    { id: 'ori_dada', category: 'Ayam Crispy Original', name: 'Dada Original', price: 12000 },
-    { id: 'ori_pahaatas', category: 'Ayam Crispy Original', name: 'Paha Atas Original', price: 12000 },
+    // AYAM ORIGINAL
+    { id: 'ori_sayap', category: 'Ayam Original', name: 'Sayap Original', price: 9000 },
+    { id: 'ori_pahabawah', category: 'Ayam Original', name: 'Paha Bawah Original', price: 10000 },
+    { id: 'ori_dada', category: 'Ayam Original', name: 'Dada Original', price: 12000 },
+    { id: 'ori_pahaatas', category: 'Ayam Original', name: 'Paha Atas Original', price: 12000 },
 
     // PAKET ORIGINAL
-    { id: 'paket_ori_sayap', category: 'Paket Ayam Crispy Original', name: 'Paket Sayap + Nasi Original', price: 12000 },
-    { id: 'paket_ori_pahabawah', category: 'Paket Ayam Crispy Original', name: 'Paket Paha Bawah + Nasi Original', price: 13000 },
-    { id: 'paket_ori_dada', category: 'Paket Ayam Crispy Original', name: 'Paket Dada + Nasi Original', price: 15000 },
-    { id: 'paket_ori_pahaatas', category: 'Paket Ayam Crispy Original', name: 'Paket Paha Atas + Nasi Original', price: 15000 },
+    { id: 'paket_ori_sayap', category: 'Paket Ayam Original', name: 'Paket Sayap + Nasi Original', price: 12000 },
+    { id: 'paket_ori_pahabawah', category: 'Paket Ayam Original', name: 'Paket Paha Bawah + Nasi Original', price: 13000 },
+    { id: 'paket_ori_dada', category: 'Paket Ayam Original', name: 'Paket Dada + Nasi Original', price: 15000 },
+    { id: 'paket_ori_pahaatas', category: 'Paket Ayam Original', name: 'Paket Paha Atas + Nasi Original', price: 15000 },
 
     // CRISPY ARABIC
-    { id: 'ar_sayap', category: 'Ayam Crispy Arabic', name: 'Sayap Arabic', price: 10000 },
-    { id: 'ar_pahabawah', category: 'Ayam Crispy Arabic', name: 'Paha Bawah Arabic', price: 11000 },
-    { id: 'ar_dada', category: 'Ayam Crispy Arabic', name: 'Dada Arabic', price: 13000 },
-    { id: 'ar_pahaatas', category: 'Ayam Crispy Arabic', name: 'Paha Atas Arabic', price: 13000 },
+    { id: 'ar_sayap', category: 'Ayam Arabic', name: 'Sayap Arabic', price: 10000 },
+    { id: 'ar_pahabawah', category: 'Ayam Arabic', name: 'Paha Bawah Arabic', price: 11000 },
+    { id: 'ar_dada', category: 'Ayam Arabic', name: 'Dada Arabic', price: 13000 },
+    { id: 'ar_pahaatas', category: 'Ayam Arabic', name: 'Paha Atas Arabic', price: 13000 },
 
     // PAKET ARABIC
-    { id: 'paket_ar_sayap', category: 'Paket Ayam Crispy Arabic', name: 'Paket Sayap + Nasi Arabic', price: 13000 },
-    { id: 'paket_ar_pahabawah', category: 'Paket Ayam Crispy Arabic', name: 'Paket Paha Bawah + Nasi Arabic', price: 14000 },
-    { id: 'paket_ar_dada', category: 'Paket Ayam Crispy Arabic', name: 'Paket Dada + Nasi Arabic', price: 16000 },
-    { id: 'paket_ar_pahaatas', category: 'Paket Ayam Crispy Arabic', name: 'Paket Paha Atas + Nasi Arabic', price: 16000 },
+    { id: 'paket_ar_sayap', category: 'Paket Ayam Arabic', name: 'Paket Sayap + Nasi Arabic', price: 13000 },
+    { id: 'paket_ar_pahabawah', category: 'Paket Ayam Arabic', name: 'Paket Paha Bawah + Nasi Arabic', price: 14000 },
+    { id: 'paket_ar_dada', category: 'Paket Ayam Arabic', name: 'Paket Dada + Nasi Arabic', price: 16000 },
+    { id: 'paket_ar_pahaatas', category: 'Paket Ayam Arabic', name: 'Paket Paha Atas + Nasi Arabic', price: 16000 },
 
     // AYAM UTUH
     { id: 'utuh', category: 'Ayam Utuh', name: 'Ayam Utuh Kecil', price: 40000 },
     { id: 'utuh_ar', category: 'Ayam Utuh', name: 'Ayam Utuh Besar', price: 55000 },
 
-    // KULIT dan USUS Ayam
-    { id: 'kulit', category: 'Kulit dan Usus', name: 'Kulit Ayam', price: 5000 },
-    { id: 'usus', category: 'Kulit dan Usus', name: 'Usus Ayam', price: 5000 },
-
     // ADD-ON
     { id: 'add_nasi', category: 'Tambahan', name: 'Nasi', price: 4000 },
+    { id: 'kulit', category: 'Tambahan', name: 'Kulit Ayam', price: 5000 },
+    { id: 'usus', category: 'Tambahan', name: 'Usus Ayam', price: 5000 },
     { id: 'add_geprek', category: 'Tambahan', name: 'Sambal Geprek', price: 4000 },
     { id: 'add_blackpepper', category: 'Tambahan', name: 'Sambal Black Pepper', price: 4000 },
     { id: 'add_keju', category: 'Tambahan', name: 'Keju', price: 4000 },
