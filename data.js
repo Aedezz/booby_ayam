a6a7539ad4facec28c8b91448fd2803592feea03
Aedezz <wafielegend2017@gmail.com>
2026-08-27 21:10:59 +0800

    ganti

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 const menuData = [
     // CRISPY ORIGINAL
-    { id: 'ori_sayap', category: 'Ayam Crispy Original', name: 'Sayap', price: 9000 },
-    { id: 'ori_pahabawah', category: 'Ayam Crispy Original', name: 'Paha Bawah', price: 10000 },
-    { id: 'ori_dada', category: 'Ayam Crispy Original', name: 'Dada', price: 12000 },
-    { id: 'ori_pahaatas', category: 'Ayam Crispy Original', name: 'Paha Atas', price: 12000 },
+    { id: 'ori_sayap', category: 'Ayam Crispy Original', name: 'Sayap Original', price: 9000 },
+    { id: 'ori_pahabawah', category: 'Ayam Crispy Original', name: 'Paha Bawah Original', price: 10000 },
+    { id: 'ori_dada', category: 'Ayam Crispy Original', name: 'Dada Original', price: 12000 },
+    { id: 'ori_pahaatas', category: 'Ayam Crispy Original', name: 'Paha Atas Original', price: 12000 },
 
     // PAKET ORIGINAL
-    { id: 'paket_ori_sayap', category: 'Paket Ayam Crispy Original', name: 'Paket Sayap + Nasi', price: 12000 },
-    { id: 'paket_ori_pahabawah', category: 'Paket Ayam Crispy Original', name: 'Paket Paha Bawah + Nasi', price: 13000 },
-    { id: 'paket_ori_dada', category: 'Paket Ayam Crispy Original', name: 'Paket Dada + Nasi', price: 15000 },
-    { id: 'paket_ori_pahaatas', category: 'Paket Ayam Crispy Original', name: 'Paket Paha Atas + Nasi', price: 15000 },
+    { id: 'paket_ori_sayap', category: 'Paket Ayam Crispy Original', name: 'Paket Sayap + Nasi Original', price: 12000 },
+    { id: 'paket_ori_pahabawah', category: 'Paket Ayam Crispy Original', name: 'Paket Paha Bawah + Nasi Original', price: 13000 },
+    { id: 'paket_ori_dada', category: 'Paket Ayam Crispy Original', name: 'Paket Dada + Nasi Original', price: 15000 },
+    { id: 'paket_ori_pahaatas', category: 'Paket Ayam Crispy Original', name: 'Paket Paha Atas + Nasi Original', price: 15000 },
 
     // CRISPY ARABIC
-    { id: 'ar_sayap', category: 'Ayam Crispy Arabic', name: 'Sayap', price: 10000 },
-    { id: 'ar_pahabawah', category: 'Ayam Crispy Arabic', name: 'Paha Bawah', price: 11000 },
-    { id: 'ar_dada', category: 'Ayam Crispy Arabic', name: 'Dada', price: 13000 },
-    { id: 'ar_pahaatas', category: 'Ayam Crispy Arabic', name: 'Paha Atas', price: 13000 },
+    { id: 'ar_sayap', category: 'Ayam Crispy Arabic', name: 'Sayap Arabic', price: 10000 },
+    { id: 'ar_pahabawah', category: 'Ayam Crispy Arabic', name: 'Paha Bawah Arabic', price: 11000 },
+    { id: 'ar_dada', category: 'Ayam Crispy Arabic', name: 'Dada Arabic', price: 13000 },
+    { id: 'ar_pahaatas', category: 'Ayam Crispy Arabic', name: 'Paha Atas Arabic', price: 13000 },
 
     // PAKET ARABIC
-    { id: 'paket_ar_sayap', category: 'Paket Ayam Crispy Arabic', name: 'Paket Sayap + Nasi', price: 13000 },
-    { id: 'paket_ar_pahabawah', category: 'Paket Ayam Crispy Arabic', name: 'Paket Paha Bawah + Nasi', price: 14000 },
-    { id: 'paket_ar_dada', category: 'Paket Ayam Crispy Arabic', name: 'Paket Dada + Nasi', price: 16000 },
-    { id: 'paket_ar_pahaatas', category: 'Paket Ayam Crispy Arabic', name: 'Paket Paha Atas + Nasi', price: 16000 },
+    { id: 'paket_ar_sayap', category: 'Paket Ayam Crispy Arabic', name: 'Paket Sayap + Nasi Arabic', price: 13000 },
+    { id: 'paket_ar_pahabawah', category: 'Paket Ayam Crispy Arabic', name: 'Paket Paha Bawah + Nasi Arabic', price: 14000 },
+    { id: 'paket_ar_dada', category: 'Paket Ayam Crispy Arabic', name: 'Paket Dada + Nasi Arabic', price: 16000 },
+    { id: 'paket_ar_pahaatas', category: 'Paket Ayam Crispy Arabic', name: 'Paket Paha Atas + Nasi Arabic', price: 16000 },
 
     // AYAM UTUH
     { id: 'utuh', category: 'Ayam Utuh', name: 'Ayam Utuh Kecil', price: 40000 },
